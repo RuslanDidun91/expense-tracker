5b27f51c95a76339e776a1fb6e7778be08adfd5d
RuslanDidun <ruslandidun5@gmail.com>
2023-02-18 14:11:29 -0700

    graph component

---
+++ client/src/components/Graph.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Chart, ArcElement } from 'chart.js'
+import { Doughnut } from 'react-chartjs-2';
+
+Chart.register(ArcElement);
+
+
+const config = {
+  data: {
+    datasets: [{
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4,
+      borderRadius: 30,
+      spacing: 10
+    }]
+  },
+  options: {
+    cutout: 115
+  }
+}
+
+
+export const Graph = () => {
+  return(
+    <div className="flex justify-content max-w-xs mx-auto">
+      <div className="item">
+        <div className="chart relative">
+          <Doughnut {...config}/>
+          <h3 className='mb-4 font-bold title'>Total
+            <span className='block text-3xl text-emerald-400'>${getTotal(data) ?? 0}</span>
+          </h3>
+        </div>
+
+        <div className="flex flex-col py-10 gap-4">
+          {/* Labels */}
+        </div>
+      </div>
+    </div>
+  )
+}
