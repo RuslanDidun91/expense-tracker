1a865058412e5832bbcc5883284b91ecdc1f89f5
RuslanDidun <ruslandidun5@gmail.com>
2023-02-22 13:55:27 -0700

    get, create transaction

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,10 @@
 const routes = require('express').Router();
 const controller = require('../controllers/controller');
 
-routes.route('/api/categories').get(controller.createCategories);
+routes.route('/api/categories')
+.post(controller.createCategories)
+.get(controller.getCategories)
+.post(controller.createCategories)
+
 
 module.exports = routes
