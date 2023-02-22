39c2b83f727a29efcba993cdf9b8034955c8f86b
RuslanDidun <ruslandidun5@gmail.com>
2023-02-22 14:29:59 -0700

    create, get, delete transaction

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,19 @@
 const routes = require('express').Router();
 const controller = require('../controllers/controller');
 
+//categories route
 routes.route('/api/categories')
-.post(controller.createCategories)
-.get(controller.getCategories)
-.post(controller.createCategories)
+  .post(controller.createCategories)
+  .get(controller.getCategories)
 
+//transactions route
+routes.route('/api/transaction')
+  .post(controller.createTransaction)
+  .get(controller.getTransaction)
+  .delete(controller.deleteTransaction)
+
+//agrigate route
+routes.route('/api/labels')
+  .get(controller.getLabels)
 
 module.exports = routes
